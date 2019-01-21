ffc103a30c384b2c547f3fb9ac4879acefdfa838
Eduard Nistru <edi@wearewip.com>
2019-01-21 14:26:54 +0200

    Main page & first version of site

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -39,38 +39,39 @@ export default class Header extends Component {
               <Link activeClassName={style.active} onClick={this.toggleSidebar} className={style.logo} href="/">
                 <img src="assets/images/nima_logo.png" />
               </Link>
-              <nav>
+              <div class={style.more__to__come}>More to come.<br /> Make sure you follow us!</div>
+              {/* <nav>
                 <Link activeClassName={style.active} onClick={this.toggleSidebar} href="/about">About Us</Link>
                 <Link activeClassName={style.active} onClick={this.toggleSidebar} href="/residents">Meet the Residents</Link>
                 <Link activeClassName={style.active} onClick={this.toggleSidebar} href="/campaigns">Campaigns</Link>
                 <Link activeClassName={style.active} onClick={this.toggleSidebar} href="/get-involved">Get Involved</Link>
-              </nav>
+              </nav> */}
               <footer>
                 <div class={style.icons}>
                   <div class={style.contact}>
-                    <a href="/">
+                    <a href="mailto:contact@sanctuarnima.ro">
                       <img src="assets/icons/mail.png" />
                     </a>
-                    <a href="/">
+                    <a href="tel:+40764166966">
                       <img src="assets/icons/phone.png" />
                     </a>
-                    <a href="/">
+                    <a href="https://goo.gl/maps/17qtt1LKnMs" target="__blank">
                       <img src="assets/icons/location.png" />
                     </a>
                   </div>
                   <div class={style.contact}>
-                    <a href="/">
-                      <img src="assets/icons/facebook.png" />
+                    <a href="https://www.facebook.com/NimaSanctuary/">
+                      <img src="assets/icons/facebook.png" target="__blank" />
                     </a>
-                    <a href="/">
+                    <a href="https://www.instagram.com/nimasanctuary/" target="__blank">
                       <img src="assets/icons/instagram.png" />
                     </a>
-                    <a href="/">
+                    <a href="https://www.youtube.com/channel/UCvPstz0mFSA5HubBD0sHmJw" target="__blank">
                       <img src="assets/icons/youtube.png" />
                     </a>
                   </div>
                   <div class={style.contact}>
-                    <a href="/">
+                    <a href="https://www.paypal.me/nimasanctuary" target="__blank">
                       <img src="assets/icons/paypal.png" />
                     </a>
                 </div>
@@ -88,38 +89,38 @@ export default class Header extends Component {
           <Link activeClassName={style.active} className={style.logo} href="/">
             <img src="assets/images/nima_logo.png" />
           </Link>
-          <nav>
+          {/* <nav>
             <Link activeClassName={style.active} href="/about">About Us</Link>
             <Link activeClassName={style.active} href="/residents">Meet the Residents</Link>
             <Link activeClassName={style.active} href="/campaigns">Campaigns</Link>
             <Link activeClassName={style.active} href="/get-involved">Get Involved</Link>
-          </nav>
+          </nav> */}
           <footer>
             <h1>Find us</h1>
             <div class={style.contact}>
-              <a href="/">
+              <a href="mailto:contact@sanctuarnima.ro">
                 <img src="assets/icons/mail.png" />
               </a>
-              <a href="/">
+              <a href="tel:+40764166966">
                 <img src="assets/icons/phone.png" />
               </a>
-              <a href="/">
+              <a href="https://goo.gl/maps/17qtt1LKnMs" target="__blank">
                 <img src="assets/icons/location.png" />
               </a>
             </div>
             <div class={style.contact}>
-              <a href="/">
-                <img src="assets/icons/facebook.png" />
+              <a href="https://www.facebook.com/NimaSanctuary/">
+                <img src="assets/icons/facebook.png" target="__blank" />
               </a>
-              <a href="/">
+              <a href="https://www.instagram.com/nimasanctuary/" target="__blank">
                 <img src="assets/icons/instagram.png" />
               </a>
-              <a href="/">
+              <a href="https://www.youtube.com/channel/UCvPstz0mFSA5HubBD0sHmJw" target="__blank">
                 <img src="assets/icons/youtube.png" />
               </a>
             </div>
             <div class={style.contact}>
-              <a href="/">
+              <a href="https://www.paypal.me/nimasanctuary" target="__blank">
                 <img src="assets/icons/paypal.png" />
               </a>
             </div>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,32 @@
-import { h } from 'preact';
+import { h, Component} from 'preact';
 import style from './style';
 
-const Home = () => (
-	<div class={style.main__container}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
-
-export default Home;
+export default class Home extends Component {
+  render() {
+    return(
+      <div class={style.main__container}>
+        <div class={style.home__header}>
+          {/* <img src="assets/images/nima_cover.jpg" /> */}
+          <span id={style.header__overlay}></span>
+          <div class={style.header__content}>
+            <div class={style.first__part}>
+              <div class={style.header__text}>
+                <h1>Nima Sanctuary</h1>
+                <h2>A safe heaven for farm animals</h2>
+              </div>
+            </div>
+            <div class={style.secont__part}>
+              <div class={style.header__buttons}>
+                <div class={style.header__text}>
+                  <h2>The first of its kind in Romania</h2>
+                </div>
+                {/* <button>HOW TO HELP</button> */}
+                <a href="https://www.paypal.me/nimasanctuary" target="__blank" id={style.donate__btn}>DONATE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
